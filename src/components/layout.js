1ba74ea630c55122607ef390b385379a59738bb0
Marty McGee <support@companyjuice.com>
2021-03-09 02:54:43 -0800

    v1.0.0 layout footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -35,11 +35,11 @@ const Layout = ({ isHomePage, children }) => {
       <main>{children}</main>
 
       <footer>
-        © {new Date().getFullYear()}, Built with
+        © {new Date().getFullYear()} Company Juice, built with
         {` `}
         <a href="https://www.gatsbyjs.com">Gatsby</a>
         {` `}
-        And <a href="https://wordpress.org/">WordPress</a>
+        + <a href="https://wordpress.org/">WordPress</a>
       </footer>
     </div>
   )
